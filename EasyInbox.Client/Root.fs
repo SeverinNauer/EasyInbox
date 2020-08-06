8139d3c911006d82bd99ab116c1b6f4c87fbedc6
Severin Nauer <severin.nauer@gmail.com>
2020-08-06 11:15:53 +0200

    Add Client project

---
+++ EasyInbox.Client/Root.fs
@@ -0,0 +1,40 @@
+﻿namespace EasyInbox.Client
+
+open Avalonia.Controls
+open Avalonia.FuncUI.DSL
+open Avalonia
+open Avalonia.Media
+open Avalonia.Controls.Shapes
+open Avalonia.Input
+
+module MenuItems = 
+    let testItems: Avalonia.FuncUI.Types.IView list = [
+        MenuItem.create [
+            MenuItem.header "Test"
+        ]
+    ]
+
+module Root = 
+    
+    type Page =
+        | Settings
+
+    let init = Settings
+
+    type Msg = 
+        Navigate of Page
+
+    let update msg state : Page = 
+        match msg with 
+        | Navigate p -> p
+
+    let view (state: Page) dispatch = 
+        Grid.create [
+            
+            Grid.children [
+                TextBlock.create [
+                    TextBlock.text "Test"
+                ]
+            ]
+        ]
+        
